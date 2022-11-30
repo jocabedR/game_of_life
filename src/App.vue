aaa53a5218e8e375881fff999dd6c5ba686f2c6f
Jocabed Ríos <j0c4b3d@gmail.com>
2022-11-30 14:41:01 -0600

    Fronted almost finished.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Game of life</h1>
+  <form class="options" @submit.prevent="setGrid">
+    Width: <input class="number-input" type="number"  v-model="width" /> 
+    <br>Height: <input class="number-input" type="number" v-model="height"/> 
+    <br>Probability: <input type="number" min="0" max="1" step="0.1" v-model="probability"/>
+    <br><button>Play!</button>
+  </form>
+  
+  <table>
+    <tr v-for="(row, rowKey) in cells" :key="rowKey">
+      <td v-for="(col, colKey) in row" :key="colKey" 
+      :class="col.alive ? 'alive' : 'dead'" class="cell"
+      ></td>
+    </tr>
+  </table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+let id = 0;
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+
+  data() {
+    return{
+      width: 0,
+      height: 0, 
+      probability: 0.0,
+      cells: []
+    }
+  },
+
+  methods:{
+    setGrid() {
+      this.cells = []
+      id = 0
+      for(let i=0; i<this.height; i++){
+        this.cells[i] = []
+        for (let j = 0; j < this.width; j++) {
+          if(this.randomNumber() > (this.probability)*10) this.cells[i][j]={ id: id++, alive: false }
+          else this.cells[i][j]={ id: id++, alive: true }
+        }
+      }
+
+    },
+    changeState(cell, i,j) {
+      this.cells[i][j].alive = !cell.alive
+      console.log(cell)
+    },
+    
+    randomNumber() {
+      return Math.floor(Math.random() * (10 - 1 + 1)) + 1
+    }
+
   }
 }
 </script>
@@ -21,6 +65,32 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 3em;
 }
+
+.number-input, button{
+  margin: 0.25em
+}
+
+.cell {
+  border-style:solid;
+  padding: 0.25em;
+}
+
+/* .cell:hover {
+  background-color: gray ;
+} */
+
+.alive {
+  background-color: black !important;
+}
+
+.dead {
+  background-color: white ;
+}
+
+table {
+  margin: auto;
+}
+
 </style>
